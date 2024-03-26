0e4f30d0ad435fa7c88978aff042e017b02447d4
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-03-26 23:33:46 +0900

    init project

---
+++ src/main/java/com/example/everyminute/EveryminuteApplication.java
@@ -0,0 +1,13 @@
+package com.example.everyminute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryminuteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryminuteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/everyminute/EveryminuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.everyminute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryminuteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
