5b66e9daed5987acd7e9316326a197ae2f123002
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-03-31 22:15:14 +0900

    #22 docs: UserController Swagger 수정

--- src/main/java/com/example/everyminute/user/controller/UserController.java
+++ src/main/java/com/example/everyminute/user/controller/UserController.java
@@ -14,6 +14,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @Api(tags = "유저 API")
 @RestController
 @RequiredArgsConstructor
@@ -25,10 +27,11 @@ public class UserController {
     @Operation(summary = "회원가입", description = "회원가입을 한다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "(S0001)회원가입 성공"),
-            @ApiResponse(responseCode = "409", description = "(U0003)존재하는 이메일입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
+            @ApiResponse(responseCode = "400", description = "(U0009)이미 사용중인 이메일입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
     })
     @PostMapping("/join")
-    public ResponseCustom join(@RequestBody JoinReq joinReq) {
+    public ResponseCustom join(@RequestBody @Valid JoinReq joinReq)
+    {
         userService.join(joinReq);
         return ResponseCustom.OK();
     }
@@ -36,11 +39,12 @@ public class UserController {
     @Operation(summary = "로그인", description = "로그인을 한다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "(S0001)로그인 성공"),
-            @ApiResponse(responseCode = "409", description = "(U0003) 존재하지 않는 이메일입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
-            @ApiResponse(responseCode = "409", description = "(U0004) 패스워드가 일치하지 않습니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
+            @ApiResponse(responseCode = "404", description = "(U0001) 존재하지 않는 유저입니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class))),
+            @ApiResponse(responseCode = "400", description = "(U0004) 패스워드가 일치하지 않습니다.", content = @Content(schema = @Schema(implementation = ResponseCustom.class)))
     })
     @PostMapping("/login")
-    public ResponseCustom<TokenDto> login(@RequestBody LoginReq loginReq) {
+    public ResponseCustom<TokenDto> login(@RequestBody @Valid LoginReq loginReq)
+    {
         return ResponseCustom.OK(userService.login(loginReq));
     }
 }
