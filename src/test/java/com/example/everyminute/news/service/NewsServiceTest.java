f2d5aa704ad69abe99e206856ba62063769b8010
dangnak2 <80161984+dangnak2@users.noreply.github.com>
2024-04-01 15:28:24 +0900

    #30 test: 소식 등록 API 실패 - 제목이 중복될 경우 테스트 코드 구현

--- src/test/java/com/example/everyminute/news/service/NewsServiceTest.java
+++ src/test/java/com/example/everyminute/news/service/NewsServiceTest.java
@@ -25,6 +25,7 @@ import static com.example.everyminute.news.dto.TestNewsDto.setUpNews;
 import static com.example.everyminute.news.dto.TestNewsDto.setUpPostNewsReq;
 import static com.example.everyminute.school.dto.TestSchoolDto.setUpSchool;
 import static com.example.everyminute.user.dto.TestUserDto.setUpUser;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -59,6 +60,25 @@ class NewsServiceTest {
         verify(schoolRepository, times(1)).findBySchoolIdAndIsEnable(any(Long.class), any(Boolean.class));
         verify(newsRepository, times(1)).existsByTitleAndIsEnable(any(String.class), any(Boolean.class));
         verify(newsRepository, times(1)).save(any(News.class));
+    }
+
+    @Test
+    @DisplayName("[실패] 관리자에 의한 소식 등록 - 제목이 중복될 경우")
+    void postNewsByAdminFail() {
+        // given
+        User user = setUpUser(14L, Role.ADMIN, "test");
+        School school = setUpSchool(7L, "명지대학교", "서울");
+        PostNewsReq req = setUpPostNewsReq("명지대학교 소식 1", "명지대학교 소식 1 입니다.");
+
+        // when
+        doReturn(Optional.of(school)).when(schoolRepository).findBySchoolIdAndIsEnable(school.getSchoolId(), true);
+        doReturn(true).when(newsRepository).existsByTitleAndIsEnable(req.getTitle(), true);
+        BaseException exception = assertThrows(BaseException.class, () -> {
+            newsService.postNewsByAdmin(user, school.getSchoolId(), req);
+        });
+
+        // then
+        assertThat(exception.getBaseResponseCode()).isEqualTo(BaseResponseCode.INVALID_NEWS_TITLE);
     }
 
     @Test
